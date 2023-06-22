8d087394b131bac2aa1dc47e8dd38732e70730bc
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 15:01:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,32 +13,25 @@ const addcostRouter = require('./routes/addcost');
 const reportRouter = require('./routes/report');
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/addcost', addcostRouter);
 app.use('/report', reportRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
