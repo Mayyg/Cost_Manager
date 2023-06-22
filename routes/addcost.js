a7cc49827392a4c95483ea5bd3e9ebd5b4da9b49
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 19:59:19 +0300

    Update addcost.js

--- routes/addcost.js
+++ routes/addcost.js
@@ -24,20 +24,21 @@ const usersModel = require("../models/users");
 const addCostRouter = express.Router(); 
 
 addCostRouter.post("/", async (req, res, next) => { 
-  const { user_id, year, month, day, description, category, sum } = req.body; 
+  const { user_id, year, month, day, description, category, sum } = req.body;
   try {
     let id = (new Date()).getTime();
-    const cost = await new costsModel({ id, user_id, year, month, day, description, category, sum }).save(); 
-    res.json({
-      id: cost.id,
-      user_id: cost.user_id,
-      year: cost.year,
-      month: cost.month,
-      day: cost.day,
-      description: cost.description,
-      category: cost.category,
-      sum: cost.sum
+   
+   const newCost = new Cost({
+      user_id,
+      year,
+      month,
+      day,
+      description,
+      category,
+      sum
     });
+    const savedCost = await newCost.save();
+     res.json(savedCost);
   } catch (error) {
     next(error); 
   }
