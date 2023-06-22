31380fc3d7a86094a454684014112a2d9d818786
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 19:16:28 +0300

    Update addcost.js

--- routes/addcost.js
+++ routes/addcost.js
@@ -1,38 +1,22 @@
 //Amit Maor 315406710
 //May Gabay 322621590
-const express = require('express');
-const addCostRouter = express.Router();
-const Cost = require("../models/costs");
-const User = require("../models/users");
+const express = require("express");
+const Costs = require("../models/costs");
+const Users = require("../models/users");
 const categories = ["food", "health", "housing", "sport", "education", "transportation", "other"];
+const addCostRouter = express.Router();
 
-addCostRouter.post("/", async (req, res) => {
+addCostRouter.post("/", async (req, res, next) => {
+    const { user_id } = req.body;
     try {
-        const { user_id, year, month, day, description, category, sum } = req.body;
-        const id = Math.floor(Math.random() * 1000000);
-        
         if (!categories.includes(category)) {
             return res.status(400).json({ error: "Invalid category" });
         }
-        
-        const newCost = {
-            id,
-            user_id,
-            year,
-            month,
-            day,
-            description,
-            category,
-            sum,
-        };
-
-        const savedCost = await Cost.create(newCost);
-        res.json(savedCost);
+        let id = Math.floor(Math.random() * 1000000);
+        const cost = await new Costs({...req.body, id}).save();
+        res.json(cost);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
+        next(error);
     }
 });
-
 module.exports = addCostRouter;
-
