490c2b7df0aba10b3a2e7e1ff3884e9990d7a835
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 16:37:08 +0300

    Update addcost.js

--- routes/addcost.js
+++ routes/addcost.js
@@ -7,41 +7,37 @@ const costs = require('../models/costs');
 const users = require('../models/users');
 
 addCostRouter.post('/', async (req, res) => {
-  try {
-    // Extract the parameters from the request body
-    const { user_id, year, month, day, description, category, sum } = req.body;
+    const { 
+        user_id, 
+        year, 
+        month, 
+        day, 
+        description, 
+        category, 
+        sum 
+    } = req.body;
 
-    // Check if the user_id exists in the users collection
-    const user = await users.findOne({ id: user_id });
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+    try {
+        const user = await User.findOne({ id: user_id });
+      
+        if(!user) {
+            res.status(400).json('user doesn\'t exists');
+            return;
+        }
+        const cost = new Cost({
+        user_id,
+        year,
+        month,
+        day,
+        description,
+        category,
+        sum,
+        });
+        const savedCost = await cost.save()
+        res.json(savedCost);
+    } catch (error) {
+        res.status(500).json("failed to add cost");
     }
-
-    // Generate a random id for the cost item
-    const id = Math.random().toString(36).substring(7);
-
-    // Create a new cost document
-    const newCost = new costs({
-      user_id,
-      year,
-      month,
-      day,
-      id,
-      description,
-      category,
-      sum
-    });
-
-    // Save the new cost document
-    const savedCost = await newCost.save();
-
-    // Return the newly added cost item as the response
-    res.json({ cost: savedCost });
-  } catch (error) {
-    // Handle any errors that occur during the process
-    res.status(500).json({ error: 'An error occurred' });
-  }
 });
 
 module.exports = addCostRouter;
-
