5809a8c3e66f2e24a407794034ee199f04fcd696
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 19:12:47 +0300

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,13 +1,26 @@
 //Amit Maor 315406710
 //May Gabay 322621590
 
+// Importing the mongoose library for MongoDB interaction
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-    id: Number,
+
+// Defining a Mongoose schema named "usersSchema" to specify the structure and data types of documents in the "users" collection in the MongoDB
+const usersSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      unique: true,
+    },
     first_name: String,
     last_name: String,
     birthday: String,
-});
+  },
+  { versionKey: false }
+);
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+// Creating a Mongoose model named "usersModel" for the "users" collection in the MongoDB database using the defined "usersSchema"
+const usersModel = mongoose.model("users", usersSchema);
+
+// Exporting the usersModel as a module
+module.exports = usersModel;
+  
