52002112b7c1f6ff3f464fde8bc3456dae432fca
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 18:47:06 +0300

    Update addcost.js

--- routes/addcost.js
+++ routes/addcost.js
@@ -1,37 +1,3 @@
 //Amit Maor 315406710
 //May Gabay 322621590
 
-const express = require('express');
-const addCostRouter = express.Router();
-const Cost = require("../models/costs");
-const User = require("../models/users");
-const categories = ["food", "health", "housing", "sport", "education", "transportation", "other"];
-
-addCostRouter.post("/addcost", async (req, res) => {
-    try {
-        const { user_id, year, month, day, description, category, sum } = req.body;
-        const id = Math.floor(Math.random() * 1000000);
-
-        // Validate the category
-        if (!categories.includes(category)) {
-            return res.status(400).json({ error: "Invalid category" });
-        }
-
-        const newCost = {
-            id,
-            user_id,
-            year,
-            month,
-            day,
-            description,
-            category,
-            sum,
-        };
-
-        const createdCost = await Cost.create(newCost);
-        res.json(createdCost);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
