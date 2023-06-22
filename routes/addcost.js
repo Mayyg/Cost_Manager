e797ab38000b3b2462ac9f783a5c7196f7e6a461
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 17:13:39 +0300

    Update addcost.js

--- routes/addcost.js
+++ routes/addcost.js
@@ -6,32 +6,27 @@ const addCostRouter = express.Router();
 const costs = require('../models/costs');
 const users = require('../models/users');
 
+addCostRouter.post('/addcost', (req, res) => {
+    const { user_id, year, month, day, description, category, sum } = req.body;
 
+    const newCost = {
+        id: 64,
+        user_id: user_id,
+        year: year,
+        month: month,
+        day: day,
+        description: description,
+        category: category,
+        sum: sum
+    };
 
-// Handling POST requests to the root path ("/") of the add cost route
-addCostRouter.post("/", async (req, res, next) => { 
-  // Extracting the user_id from the request body
-  const { user_id } = req.body; 
-    
-    try {
-        const user = await User.findOne({ id: user_id });
-      
-        if(!user) {
-            res.status(400).json('user doesn\'t exists');
-            return;
-        }
-       let id = (new Date()).getTime();
-
-
-
-    // Create a new cost document based on the request body and save it to the database
-    const cost = await new costs({...req.body, id}).save(); 
-
-    // Send a JSON response with the saved cost document and pass any caught error to the error-handling middleware
-    res.json(cost);
-    } catch (error) {
-        res.status(500).json("failed to add cost");
-    }
+    newCost.save()
+        .then((addedCost) => {
+            res.json(addedCost);
+        })
+        .catch((error) => {
+            res.status(500).json({ error: 'Failed to add the cost item.' });
+        });
 });
 
 module.exports = addCostRouter;
