3d4372a228ebd4b28d4713aa21ee15073dbee2c9
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 20:27:40 +0300

    Update users.js

--- models/users.js
+++ models/users.js
@@ -4,15 +4,11 @@
 const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema(
   {
-    id: {
-      type: Number,
-      unique: true,
-    },
+    id:Number,
     first_name: String,
     last_name: String,
     birthday: String,
   },
-  { versionKey: false }
 );
 const usersModel = mongoose.model("users", usersSchema);
 module.exports = usersModel;
