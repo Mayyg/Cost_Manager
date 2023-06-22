35598e8afd139a84e50c00d3a35aba4705b91797
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 18:05:20 +0300

    Update addcost.js

--- routes/addcost.js
+++ routes/addcost.js
@@ -6,25 +6,8 @@ const addCostRouter = express.Router();
 const costs = require('../models/costs');
 const users = require('../models/users');
 const { categories } = ["food", "health", "housing", "sport", "education", "transportation", "other"];
-addCostRouter.post("/", async (req, res) => {
-    try {
-        const { user_id, year, month, day, description, category, sum } = req.body;
-        const id = Math.floor(Math.random() * 100000);
-        const cost = new costs({
-            id,
-            user_id,
-            year,
-            month,
-            day,
-            description,
-            category,
-            sum,
-        });
-        await cost.save();
-        res.json(cost);
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
-    }
+addCostRouter.post("/", async (req, res, next) => {
+    const id = Math.floor(Math.random() * 1000000);
+    const cost = await new costs({...req.body, id}).save();
+    res.json(cost);
 });
