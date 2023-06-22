99e08950bd6d4bc3fa140bbc1f23699da7ca68d3
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 15:10:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 //Amit Maor 315406710
 //May Gabay 322621590
-const createError = require('http-errors');
+
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const compression = require('compression');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const aboutRouter = require('./routes/about');
-const addcostRouter = require('./routes/addcost');
-const reportRouter = require('./routes/report');
+const mongoose = require('mongoose');
 const app = express();
+
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const crypto = require('crypto');
@@ -28,28 +21,22 @@ app.use(express.json());
         console.log('connected to the ATLAS data base!');
     });
 
-app.set('views', path.join(__dirname, 'views'));
-app.use(logger('dev'));
+const addCostR = require('./routes/addcost.js');
+const reportR = require('./routes/report.js');
+const aboutR = require('./routes/about.js');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(compression());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/about', aboutRouter);
-app.use('/addcost', addcostRouter);
-app.use('/report', reportRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(addCostR);
+app.use(reportR);
+app.use(aboutR);
+
+app.listen(3000, () => {
+    console.log('server is up and running');
 });
 
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-module.exports = app;
