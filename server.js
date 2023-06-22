d4082760e3a2fcc1a0a08c86ac12df4a29bbfeb7
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 15:01:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,46 @@
 //Amit Maor 315406710
 //May Gabay 322621590
+const createError = require('http-errors');
 const express = require('express');
-const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const aboutRouter = require('./routes/about');
+const addcostRouter = require('./routes/addcost');
+const reportRouter = require('./routes/report');
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.json());
-const MONGODBUR = 'mongodb+srv://amitmay:amitmay1@cluster0.2xxxc54.mongodb.net/costs?retryWrites=true&w=majority';
-mongoose
-    .connect(MONGODBUR, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Failed to connect to MongoDB', error);
-    });
-
-const addCostR = require('./routes/addcost.js');
-const reportR = require('./routes/report.js');
-const aboutR = require('./routes/about.js');
-
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
 
-app.use(addCostR);
-app.use(reportR);
-app.use(aboutR);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/addcost', addcostRouter);
+app.use('/report', reportRouter);
 
-app.listen(3000, () => {
-    console.log('server is up and running');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
