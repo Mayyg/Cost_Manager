bf128316c0f8d1d5cbbb1114cccd789d92205307
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 15:17:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,35 +6,31 @@ const mongoose = require('mongoose');
 const app = express();
 mongoose.set('strictQuery', true);
 const crypto = require('crypto');
-    const connectionParams={
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-app.use(express.json());
-    mongoose.connect('mongodb+srv://amitmay:amitmay1@cluster0.2xxxc54.mongodb.net/costs?retryWrites=true&w=majority'
-        ,connectionParams);
-    const db = mongoose.connection;
-    db.on('error', () => {console.log('error connect to the server- please check your internet connection')});
-    db.once('open', () => {
-        console.log('connected to the ATLAS data base!');
-    });
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-const addCostR = require('./routes/addcost.js');
-const reportR = require('./routes/report.js');
-const aboutR = require('./routes/about.js');
+mongoose.connect('mongodb+srv://amitmay:amitmay1@cluster0.2xxxc54.mongodb.net/costs?retryWrites=true&w=majority', connectionParams)
+  .then(() => {
+    console.log('Connected to the ATLAS database!');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the server:', error);
+  });
+
+const addCostRouter = require('./routes/addcost');
+const reportRouter = require('./routes/report');
+const aboutRouter = require('./routes/about');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(addCostR);
-app.use(reportR);
-app.use(aboutR);
-
-app.listen(3000, () => {
-    console.log('server is up and running');
-});
+app.use('/addcost', addCostRouter);
+app.use('/report', reportRouter);
+app.use('/about', aboutRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
