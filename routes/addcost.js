a6bb793dd827e0f66bc0d64b934671eb0d279908
Mayga <107255992+Mayyg@users.noreply.github.com>
2023-06-22 18:25:23 +0300

    Update addcost.js

--- routes/addcost.js
+++ routes/addcost.js
@@ -6,8 +6,25 @@ const addCostRouter = express.Router();
 const costs = require('../models/costs');
 const users = require('../models/users');
 const { categories } = ["food", "health", "housing", "sport", "education", "transportation", "other"];
-addCostRouter.post("/", async (req, res, next) => {
-    const id = Math.floor(Math.random() * 1000000);
-    const cost = await new costs({...req.body, id}).save();
-    res.json(cost);
+addCostRouter.post("/addcost", (req, res) => {
+    const { user_id, year, month, day, description, category, sum } = req.body;
+        const id = Math.floor(Math.random() * 1000000);
+        const newCost = new costs({
+            id,
+            user_id,
+            year,
+            month,
+            day,
+            description,
+            category,
+            sum,
+        });
+        newCost.save((err, cost) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: "Failed to add cost item." });
+            } else {
+                res.json({ costs });
+            }
+        });
 });
